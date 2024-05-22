82588b96771d49057a3c6f50c5f8d1d32c416562
Santiago Llerena Lanchero <slanchero@uninorte.edu.co>
2024-05-21 23:08:17 -0500

    change button and name

--- public/js/page.js
+++ public/js/page.js
@@ -109,7 +109,23 @@ const showCurrentPage = (page) => {
                 ${product.rating ? '<p class="second">' + product.rating + '</p>' : ""}
             </div>
         </div>
-        <a class="button" target="_blank" href="${product.articleLink}">Ir</a>
+        <a href="${product.articleLink}" target="_blank" class="cta">
+            <span class="hover-underline-animation"> Shop now </span>
+            <svg
+                id="arrow-horizontal"
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="10"
+                viewBox="0 0 46 16"
+            >
+                <path
+                id="Path_10"
+                data-name="Path 10"
+                d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
+                transform="translate(30)"
+                ></path>
+            </svg>
+        </a>
       `;
 
         productList.appendChild(productItem);
